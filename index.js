330a82fc383f6a33f61aaaedb30b83d6cb81472f
Raul Santa Cruz <raulsantacruz0@gmail.com>
2022-04-22 20:09:01 -0500

    user route updated

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const loginRouter = require('./routes/login')
+const userRouter = require('./routes/user')
 
 const app = express()
 
@@ -8,6 +9,7 @@ require('./database')
 const PORT = 3001
 
 app.use('/auth/local/login', loginRouter)
+app.use('/api/users', userRouter)
 
 
 const server = app.listen(PORT, ()=>{
